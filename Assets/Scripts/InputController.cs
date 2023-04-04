acdb8614323eb55a198bb7074cf70dd4f997d9ab
Katharina Broswik <Katharina_B@live.de>
2023-04-04 14:08:16 +0200

    Fix main menu connection to game

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,41 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class InputController : MonoBehaviour
-{
+public class InputController : MonoBehaviour {
 
-    private PlayerController _playerController;
+    PlayerController _playerController;
 
-    private bool _jump;
-    private bool _shoot;
-    private bool _interact;
+    bool _jump;
+    bool _shoot;
+    bool _interact;
 
     // Use this for initialization
-    void Start()
-    {
+    protected void Start() {
         _playerController = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!_jump)
-        {
+    protected void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene("Title");
+        }
+
+        if (!_jump) {
             _jump = Input.GetButtonDown("Jump");
         }
-        if (!_shoot)
-        {
+        if (!_shoot) {
             _shoot = Input.GetButtonDown("Fire1");
         }
-        if(!_interact)
-        {
+        if (!_interact) {
             _interact = Input.GetButtonDown("Interact");
         }
     }
 
-    void FixedUpdate()
-    {
+    protected void FixedUpdate() {
         _playerController.HandleButtonInputs(_jump, _shoot, _interact);
 
         _jump = false;
---
+++ Assets/Scripts/MainMenuInput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInput : MonoBehaviour {
+    protected void Update() {
+        if (Input.anyKey) {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
