84380fec6d446fc97c0ff32aa10960fdf7934c63
Katharina Broswik <Katharina_B@live.de>
2023-04-04 13:44:13 +0200

    Fix trigger-borne exception

--- Assets/Scripts/HomeTriggerCheck.cs
+++ Assets/Scripts/HomeTriggerCheck.cs
@@ -1,82 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class HomeTriggerCheck : MonoBehaviour
-{
-    public Sprite sp_BlockingWallSprite;
-    public Sprite sp_PassWallSprite;
+public class HomeTriggerCheck : MonoBehaviour {
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-
-        if (collision.gameObject.tag == "BlockingWall")
-        {
-          
-                collision.gameObject.transform.parent.GetComponent<SpriteRenderer>().enabled = true;
-          
-            
+    protected void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("BlockingWall")) {
+            collision.gameObject.transform.parent.GetComponent<SpriteRenderer>().enabled = true;
             collision.gameObject.transform.parent.GetComponent<Collider2D>().enabled = true;
-
-            //collision.gameObject.transform.parent.GetComponent<SpriteRenderer>().sprite = sp_BlockingWallSprite;
         }
 
-        if (collision.gameObject.tag == "Button")
-        {
-
+        if (collision.gameObject.CompareTag("Button")) {
             collision.gameObject.transform.parent.GetComponent<BoxCollider2D>().enabled = false;
         }
 
-        if (collision.gameObject.tag == "Platform")
-        {
-            
+        if (collision.gameObject.CompareTag("Platform")) {
             collision.gameObject.transform.parent.GetComponent<BoxCollider2D>().enabled = true;
-
         }
 
-        if (collision.gameObject.tag == "Player")
-        {
+        if (collision.gameObject.CompareTag("Player")) {
             PlayerController.instance.leftTheCircle = false;
             PlayerController.instance.SetOutOfHomeState(0);
             PlayerController.instance.SetNormalModeTrigger();
         }
-
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-
-
-        if (collision.gameObject.tag == "BlockingWall")
-        {
-            
-            //collision.gameObject.transform.parent.GetComponent<SpriteRenderer>().sprite = sp_PassWallSprite;
-
+    protected void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("BlockingWall")) {
             collision.gameObject.transform.parent.GetComponent<SpriteRenderer>().enabled = false;
             collision.gameObject.transform.parent.GetComponent<Collider2D>().enabled = false;
-          
         }
 
-        if (collision.gameObject.tag == "Button")
-        {
+        if (collision.gameObject.CompareTag("Button")) {
 
             collision.gameObject.transform.parent.GetComponent<BoxCollider2D>().enabled = true;
             Debug.Log(collision.gameObject.transform.GetComponentInParent<BoxCollider2D>().gameObject.name);
         }
-        if (collision.gameObject.tag == "Platform")
-        {
-
+        if (collision.gameObject.CompareTag("Platform")) {
             collision.gameObject.transform.parent.GetComponent<BoxCollider2D>().enabled = false;
-
         }
 
-        if (collision.gameObject.tag == "Player")
-        {
+        if (collision.gameObject.CompareTag("Player")) {
             PlayerController.instance.leftTheCircle = true;
             PlayerController.instance.outOfCircleStartFrame = Time.frameCount;
         }
     }
-
 }
 
